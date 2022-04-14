fcaa57fe92c91382475440ca076151aafed81378
denjayx <denyw602@gmail.com>
2022-04-14 13:01:22 +0700

    add TaxiConsoleAp

---
+++ TaxiConsoleApp/TaxiConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Deni";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10.ToString();
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
