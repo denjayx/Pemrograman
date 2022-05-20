fcc699df80a25f915d82527399a417be87d6fa3d
denjayx <denyw602@gmail.com>
2022-05-20 09:53:56 +0700

    class Dosen

---
+++ examNumber4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosenDefault = new Dosen(1, "Kamarudin", 4539854, "Laki-laki", "Pemrograman");
+
+            dosenDefault.cetakHasil();
+            Console.WriteLine();
+            dosenDefault.addDosen();
+            dosenDefault.addCourse();
+            Console.WriteLine();
+            dosenDefault.cetakAkhir();
+
+            Console.ReadKey();
+        }
+    }
+}
