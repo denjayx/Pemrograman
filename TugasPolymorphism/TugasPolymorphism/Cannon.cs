8df5108f183f1928e423588a880a61276c6d77db
denjayx <denyw602@gmail.com>
2022-06-09 22:13:13 +0700

    Tugas Polymorphism

---
+++ TugasPolymorphism/TugasPolymorphism/Cannon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPolymorphism
+{
+    public class Cannon : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Cannon Display Dimension : 9.5*12");
+        }
+        public override void Print()
+        {
+
+            Console.WriteLine("Cannon printer printing...");
+        }
+    }
+}
---
+++ TugasPolymorphism/TugasPolymorphism/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int nomorPrinter;
+            do
+            {
+                PrinterWindows printer;
+
+                Console.WriteLine("Pilih Printer");
+                Console.WriteLine("1. Epson");
+                Console.WriteLine("2. Cannon");
+                Console.WriteLine("3. LaserJet");
+                Console.WriteLine("4. Cancle");
+
+                Console.Write("Nomor Printer [1..3] : ");
+                nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+                if (nomorPrinter == 1)
+                {
+                    printer = new Epson();
+                }
+                else if (nomorPrinter == 2)
+                {
+                    printer = new Cannon();
+                }
+                else
+                {
+                    printer = new LaserJet();
+                }
+
+                printer.Show();
+                printer.Print();
+                Console.WriteLine();
+            } while (nomorPrinter != 4);
+        }
+    }
+}
